3334951a7c6562d4c066ba61daebcf19b902cc09
gmedeiros93 <gabriel.medeiros93@gmail.com>
2020-04-29 21:10:06 -0300

    Atualizar arquivo Classificador_Terreno.java

--- josm/plugins/Quali_OSM/src/org/openstreetmap/josm/plugins/qualiosm/Classificador_Terreno.java
+++ josm/plugins/Quali_OSM/src/org/openstreetmap/josm/plugins/qualiosm/Classificador_Terreno.java
@@ -1,53 +1,37 @@
 package org.openstreetmap.josm.plugins.qualiosm;
 
-
 import static org.openstreetmap.josm.tools.I18n.tr;
-
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
-
-
-import org.openstreetmap.josm.actions.JosmAction;
-import org.openstreetmap.josm.gui.MainApplication;
-import org.openstreetmap.josm.tools.ImageProvider;
+import javax.swing.Action;
+import javax.swing.Icon;
 import org.openstreetmap.josm.tools.Shortcut;
+import org.openstreetmap.josm.actions.ToggleAction;
+import org.openstreetmap.josm.gui.layer.Layer;
+import org.openstreetmap.josm.data.imagery.ImageryInfo;
+import org.openstreetmap.josm.gui.MainApplication;
 import org.openstreetmap.josm.gui.layer.ImageryLayer;
 
+public class Classificador_Terreno extends ToggleAction {
+    
+ protected Icon icon;
 
-
-/**
- *
- * @author Gabriel
- */
-public class Classificador_Terreno extends JosmAction  {
-    
-    public boolean enableFilter = true;
-    
-    
-    public Classificador_Terreno() {
-    super(tr("Apply Terrain Classifier"), new ImageProvider("terrain.png"), tr("Apply Terrain Classifier"),
-                Shortcut.registerShortcut("Apply Terrain Classifier", tr("Apply Terrain Classifier"),
-                        KeyEvent.VK_A, Shortcut.CTRL_SHIFT), false, "ClassificaTerreno",
-                        true);
-   
-    }
-    
-   
-    @Override
-    public void actionPerformed(ActionEvent e) {
-   
  
-    FilterActivate dialog = new FilterActivate((ImageryLayer) MainApplication.getLayerManager().getActiveLayer());
-            MainApplication.getLayerManager().getActiveLayer().setFilterStateChanged();
-          
-         
-            
-  
+    public Classificador_Terreno() {
+        super(tr("Enable/Disable Terrain Classifier"),
+              null, /* no icon */
+              tr("Enable/disable terrain classifier"),
+              Shortcut.registerShortcut("menu:qualiosm:terrainclassifier", tr("Terrain Classifier"), KeyEvent.VK_W, Shortcut.CTRL),
+              false /* register toolbar */
+        );
+        putValue("toolbar", "wireframe");
+        Action register = MainApplication.getToolbar().register(this);
+       
+        notifySelectedState();
     }
-  
-     
+
     @Override
-    protected void updateEnabledState() {
+   protected void updateEnabledState() {
         if (!(MainApplication.getLayerManager().getActiveLayer() instanceof ImageryLayer)) {
             setEnabled(false);
         } else {
@@ -55,10 +39,51 @@ public class Classificador_Terreno extends JosmAction  {
         }
     }
     
-           
 
-
-   
-
+    @Override
+    public void actionPerformed(ActionEvent e) {
+       
+        toggleSelectedState(e);
+  
+      // Realiza acao se o botao estiver selecionado
+        if (isSelected()) {
+         
      
+            FilterActivate filterActivate = new FilterActivate((ImageryLayer) MainApplication.getLayerManager().getActiveLayer());    
+            MainApplication.getLayerManager().getActiveLayer().setFilterStateChanged();    
+            System.out.println("Ativado!");
+          
+  
+          
+                
+       
+        } else {
+         
+            Layer  layer = (ImageryLayer) MainApplication.getLayerManager().getActiveLayer();
+            
+                    
+            MainApplication.getLayerManager().removeLayer(layer);
+            
+        ImageryInfo info = new ImageryInfo(layer.getName(), "http://bar", "bing", null, null);
+        
+      
+        
+        
+        
+        
+      
+        Layer layer2 = ImageryLayer.create(info);
+        
+        MainApplication.getLayerManager().addLayer(layer2);
+      MainApplication.getLayerManager().setActiveLayer(layer2);
+        
+            
+   
+    System.out.println("Desativado!");      
+        }
+          
+        notifySelectedState();
+        
+}
+
 }
