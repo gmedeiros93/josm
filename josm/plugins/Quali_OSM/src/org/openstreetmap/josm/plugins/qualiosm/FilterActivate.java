2fa7328938126fe1ab7ba48dba60e670f27ef84f
gmedeiros93 <gabriel.medeiros93@gmail.com>
2020-05-03 20:51:19 -0300

    Atualizar arquivo FilterActivate.java

--- josm/plugins/Quali_OSM/src/org/openstreetmap/josm/plugins/qualiosm/FilterActivate.java
+++ josm/plugins/Quali_OSM/src/org/openstreetmap/josm/plugins/qualiosm/FilterActivate.java
@@ -1,12 +1,21 @@
+/***************************************************
+* File: FilterActivate.java
+* Author: Gabriel Franklin Braz de Medeiros
+* Programa de Pos-Graduacao em Informatica
+* University of Brasilia
+* Professor Maristela Terto de Holanda
+*****************************************************/
+
 package org.openstreetmap.josm.plugins.qualiosm;
 
 import org.openstreetmap.josm.gui.layer.ImageryLayer;
 import org.openstreetmap.josm.gui.layer.Layer;
 
-/**
- * Ativacao do Filtro do Classificador de Terrenos
- * @author Gabriel Medeiros
- */
+/***************************************************
+ * Class FilterActivate is responsible for activating
+ * the filter implemented by the terrain classifier.
+ ***************************************************/
+
 public class FilterActivate {
     private Layer layer;
     private FiltersManager filtersManager;
