f92059495e29ab1b99b4a9c3f078deeebfb7a7cf
gmedeiros93 <gabriel.medeiros93@gmail.com>
2020-05-03 20:53:25 -0300

    Atualizar arquivo FiltersManager.java

--- josm/plugins/Quali_OSM/src/org/openstreetmap/josm/plugins/qualiosm/FiltersManager.java
+++ josm/plugins/Quali_OSM/src/org/openstreetmap/josm/plugins/qualiosm/FiltersManager.java
@@ -1,4 +1,10 @@
-
+/***************************************************
+* File: FiltersManager.java
+* Author: Gabriel Franklin Braz de Medeiros
+* Programa de Pos-Graduacao em Informatica
+* University of Brasilia
+* Professor Maristela Terto de Holanda
+*****************************************************/
 package org.openstreetmap.josm.plugins.qualiosm;
 
 import java.awt.image.BufferedImage;
@@ -7,44 +13,31 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.openstreetmap.josm.tools.ImageProcessor;
 
+/**********************************************************
+Class FiltersManager is responsible for calling the methods 
+that perform image processing used by the filter.
+**********************************************************/
 
 public class FiltersManager implements ImageProcessor{
     
-   
+public FilterActivate dialog;
     
-        public FilterActivate dialog;
-    
-    public FiltersManager(FilterActivate dialog) {
+public FiltersManager(FilterActivate dialog) {
         this.dialog = dialog;
     }
    
   @Override
      public BufferedImage process(BufferedImage image) {
 
-     
-
- while(true){
-            try {
-                // iterating through map of filters according to the order
-                
-                
+while(true){
+            try {  
                 FilterTerrain filter = new FilterTerrain();
-                
-                
-                // if next filter will return null
-                // we should take an old example of the image
                 BufferedImage oldImg = image;
-                
-                
-                // Aplicacao do filtro
-                
-             
-                image = filter.filter_terrain(image);
+               image = filter.filter_terrain(image);
              
                 if (image == null) {
                     image = oldImg;
-                    
-                    
+                                     
                 }
                 
                 return image;
